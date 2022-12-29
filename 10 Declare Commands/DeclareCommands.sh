a931354d1345e27f0720998814f8e6ed5edcd616
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2022-12-29 14:46:39 +0700

    Add files via upload

---
+++ 10 Declare Commands/DeclareCommands.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+declare -r data=/home/user/Data/folder
+
+echo $data
+
+data=/home/user/Data/folder/file.txt
+
+echo $data
+
+#cat /home/user/Data/folder/file.txt
+
+
+
+# How to see Variable on TERMINAL 
+# declare -p  
+# declare myvariable=20
+# declare -p
+# declare myvariable=10
+# -r --> ReadOnly
