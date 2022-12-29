08bdde43d4b3ceec943aa8f401db8f3be1d17e3b
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2022-12-29 14:45:29 +0700

    Add files via upload

---
+++ 05 Loops/forLoop1.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+for i in {0..10..2}
+do 
+	echo $i
+done
+
+ 
+# Catatan
+# -le ==> less equal <=
+# -lt ==> less then <
+# -ge ==> greater equal >=
+# -gt ==> greater than >
+
+# until do ---> Run if condition False
+# while do ---> Run if condition True
