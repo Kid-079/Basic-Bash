f4461f912ffbf517e6c960c1bce8971b90bcbdb3
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-27 12:56:34 +0700

    SendScript2.sh

--- 08 Send Output Script to Another Script/SendScript2.sh
+++ 08 Send Output Script to Another Script/SendScript2.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 
 echo "enter 1st string"
+#echo "Love Is Wonderful And Makes You Beautiful"
+#echo "There Are So Many Beautiful Reason To Be Happy"
 read string1
 
 echo "enter 2nd string"
+#echo "Love Is Wonderful And Makes You Beautiful"
+#echo "There Are So Many Beautiful Reason To Be Happy"
 read string2
 
 if [ "$string1" == "$string2" ]
