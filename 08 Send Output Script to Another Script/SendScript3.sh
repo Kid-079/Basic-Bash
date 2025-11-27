f2526e690662f9afd4d8a26c11ab5c18060f0a34
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-27 12:59:31 +0700

    SendScript3.sh

--- 08 Send Output Script to Another Script/SendScript3.sh
+++ 08 Send Output Script to Another Script/SendScript3.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
 
 echo "enter 1st string"
+#echo "Love Is Wonderful And Makes You Beautiful"
+#echo "There Are So Many Beautiful Reason To Be Happy"
+#echo "Because Of Your Smile, You Make Life More Beautiful"
 read string1
 
 echo "enter 2nd string"
+#echo "Love Is Wonderful And Makes You Beautiful"
+#echo "There Are So Many Beautiful Reason To Be Happy"
+#echo "Because Of Your Smile, You Make Life More Beautiful"
 read string2
 
 if [ "$string1" \< "$string2" ]
