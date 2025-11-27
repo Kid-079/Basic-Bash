a29ddf600b5f29412995c2ec367b6fd91478f4b5
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-27 13:16:37 +0700

    FunctionsCheck2.sh

--- 12 Functions/FunctionsCheck2.sh
+++ 12 Functions/FunctionsCheck2.sh
@@ -3,13 +3,15 @@
 function funcCheck()
 {
 	Value="This is a String"
-
+	#Value="It's Not Always Easy, But It's Always Worth It"
+	#Value="Life Is A Journey Make The Best Of It"
 }
 
 funcCheck
 echo $Value
 
 Value="Nice String"
+#Value="Never Give Up Because Great Things Take Time"
 echo $Value
 
 
