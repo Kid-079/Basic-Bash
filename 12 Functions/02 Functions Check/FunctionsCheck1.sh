7a71f7afa67263f5204f15f302190dc81a43f30a
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-24 09:06:10 +0700

    Update FunctionsCheck1.sh

--- 12 Functions/02 Functions Check/FunctionsCheck1.sh
+++ 12 Functions/02 Functions Check/FunctionsCheck1.sh
@@ -2,7 +2,7 @@
 
 function funcCheck()
 {
-	Value="This is String"
+	Value="Work Hard In Silence"
 	#Value="It's Not Always Easy, But It's Always Worth It"
 	#Value="Life Is A Journey Make The Best Of It"
 
