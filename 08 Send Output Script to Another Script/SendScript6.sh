53faf67bcfa573e28a06401267c1b1f9d652dd1e
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-24 08:57:32 +0700

    Update SendScript6.sh

--- 08 Send Output Script to Another Script/SendScript6.sh
+++ 08 Send Output Script to Another Script/SendScript6.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-echo "enter 1st string"
+echo "Work Hard In Silence"
 #echo "Love Is Wonderful And Makes You Beautiful"
 #echo "There Are So Many Beautiful Reason To Be Happy"
 #echo "Because Of Your Smile, You Make Life More Beautiful"
 read string1
 
-echo "enter 2nd string"
+echo "Little Moments, Big Memories"
 #echo "Love Is Wonderful And Makes You Beautiful"
 #echo "There Are So Many Beautiful Reason To Be Happy"
 #echo "Because Of Your Smile, You Make Life More Beautiful"
